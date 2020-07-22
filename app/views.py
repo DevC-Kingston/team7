5051783a8bd90c943aa02e7b2f5af5141c4d2ed7
Kimski <k_oksana15@hotmail.com>
2020-07-22 18:22:19 -0500

    handle receiving msg

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ import urllib
 import json
 import traceback
 from flask import request, render_template
+from credentials import credentials
+
 
 
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
@@ -11,11 +13,8 @@ VERIFY_TOKEN = '5NclPz4kdN0cX06Hy+aHzaPM8zRyoI3Xgb4NXJjtTCs='
 PAGE_ACCESS_TOKEN = 'EAAKSLLFWS2gBABZBd7p8Dpndc3K533G4J33e8zBhEneiMbUvMnxBWKfzTUXRcMyZA5zf8MniYFcZCrjLWm2nVZAhZBIVsmAXZCLoH2KK6UU3jBnq0bNUGytxSPaGFBY2Qa8XFIbYno70qBizZA1qK7FB6ZBplbLlWTSEjC0Ww84W9wZDZD'
 
 
-def hello_world():
-    return 'Hello, World!'
 
-
-@app.route('/webhook')
+@app.route('/webhook', methods = ['GET'])
 def listen():
     """This is the main function flask uses to 
     listen at the `/webhook` endpoint"""
@@ -38,30 +37,65 @@ def verify_webhook(req):
     if req.args.get("hub.verify_token") == VERIFY_TOKEN:
         return req.args.get("hub.challenge")
     else:
-        return "incorrect"
+        return "incorrect again"
 
-def send_message(recipient_id, text):
-    """Send a response to Facebook"""
-    payload = {
-        'message': {
-            'text': text
-        },
-        'recipient': {
-            'id': recipient_id
-        },
-        'notification_type': 'regular'
-    }
+# def send_message(recipient_id, text):
+#     """Send a response to Facebook"""
+#     payload = {
+#         'message': {
+#             'text': text
+#         },
+#         'recipient': {
+#             'id': recipient_id
+#         },
+#         'notification_type': 'regular'
+#     }
 
-    auth = {
-        'access_token': PAGE_ACCESS_TOKEN
-    }
+#     auth = {
+#         'access_token': PAGE_ACCESS_TOKEN
+#     }
 
-    response = requests.post(
-        FB_API_URL,
-        params=auth,
-        json=payload
-    )
+#     response = requests.post(
+#         FB_API_URL,
+#         params=auth,
+#         json=payload
+#     )
 
-    response.json()
+#     response.json()
 
 
+@app.route("/", methods=['GET', 'POST'])
+def rec_message(text):
+    if request.method == 'GET':
+        """confirms all requests the bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_webhook(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        output  = request.get_json()
+        for e in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+        return "Message Processed"
+
+def send_message(sender_id, message_text):
+    '''
+    Sending response back to the user using facebook graph API
+    '''
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": PAGE_ACCESS_TOKEN},
+    headers={"Content-Type": "application/json"},
+    data=json.dumps({
+        "recipient": {"id": sender_id},
+        "message": {"text": message_text}
+    }))

8c5a4c6fedc9c0b5d7964619fd88a485cf300df4
Kimski <k_oksana15@hotmail.com>
2020-07-23 22:21:16 -0500

    testing

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
+
 from app import app
-from flask import Flask
+from flask import Flask, request
 import urllib
 import json
 import traceback
-from flask import request, render_template
+import requests
+from credentials import credentials
+from pymessenger import Bot
 
 
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
@@ -11,37 +14,63 @@ VERIFY_TOKEN = '5NclPz4kdN0cX06Hy+aHzaPM8zRyoI3Xgb4NXJjtTCs='  # openssl rand -b
 PAGE_ACCESS_TOKEN = 'EAAKSLLFWS2gBABZBd7p8Dpndc3K533G4J33e8zBhEneiMbUvMnxBWKfzTUXRcMyZA5zf8MniYFcZCrjLWm2nVZAhZBIVsmAXZCLoH2KK6UU3jBnq0bNUGytxSPaGFBY2Qa8XFIbYno70qBizZA1qK7FB6ZBplbLlWTSEjC0Ww84W9wZDZD'
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-def send_message(recipient_id, text):
-    """Send a response to Facebook"""
-    payload = {
-        'message': {
-            'text': text
-        },
-        'recipient': {
-            'id': recipient_id
-        },
-        'notification_type': 'regular'
-    }
-
-    auth = {
-        'access_token': PAGE_ACCESS_TOKEN
-    }
-
-    response = requests.post(
-        FB_API_URL,
-        params=auth,
-        json=payload
-    )
-
-    response.json()
+"""The listen() function handles these http requests and 
+checks that they contain a valid Facebook message"""
 
 
+@app.route('/webhook', methods=['GET', 'POST'])
+def listen():
+    """This is the main function flask uses to 
+    listen at the `/webhook` endpoint"""
+    if request.method == 'GET':
+        return verify_webhook(request)
+
+    if request.method == 'POST':
+        payload = request.json
+        event = payload['entry'][0]['messaging']
+        for x in event:
+            if is_user_message(x):
+               text = x['message']['text']
+               sender_id = x['sender']['id']
+
+            #    if text == "covnews":
+            #        return covnews(sender_id)
+            #    elif text == "pil":
+            #       return pil(sender_id)
+            #    else:
+            #        response = "I am sorry, I do not understand. Please enter "'covnews'" or "'pil'" to continue"
+            return respond(sender_id, response)
+              #respond(sender_id, text)
+        return "ok"
+
+
+"""What Facebook use to verify the right server"""
 def verify_webhook(req):
     if req.args.get("hub.verify_token") == VERIFY_TOKEN:
         return req.args.get("hub.challenge")
     else:
-        return "incorrect"
+        return "incorrect again"
+
+
+"""Check if the message is a message from the user"""
+
+
+def is_user_message(message):
+    return (message.get('message') and
+            message['message'].get('text') and
+            not message['message'].get("is_echo"))
+
+
+"""Formulate a response to the user and pass it on to a function that sends it."""
+def respond(sender, message):
+    response = get_bot_response(message)
+    send_message(sender, message)
+    return "ok"
+
+
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
